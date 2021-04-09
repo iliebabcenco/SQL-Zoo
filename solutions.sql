838c8ab38c17f33d0274770dca6f574790589b92
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-09 13:43:39 +0300

    Add SELECT within SELECT Tutorial solutions

--- solutions.sql
+++ solutions.sql
@@ -119,3 +119,46 @@ FROM nobel
 WHERE yr = 1984
 ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner
 
+--SELECT within SELECT Tutorial
+
+SELECT name FROM world
+WHERE population >
+(SELECT population FROM world
+WHERE name='Russia')
+
+SELECT name FROM world 
+WHERE (gdp / population) > (SELECT gdp / population FROM world WHERE name = 'United Kingdom') 
+AND continent = 'Europe'
+
+SELECT name, continent FROM world 
+WHERE continent = (SELECT continent FROM world WHERE name = 'Argentina') 
+OR continent = (SELECT continent FROM world WHERE name = 'Australia') ORDER BY name
+
+SELECT name, population FROM world 
+WHERE population > (SELECT population FROM world WHERE name = 'Canada') 
+AND population < (SELECT population FROM world WHERE name = 'Poland')
+
+SELECT name, CONCAT(CAST(ROUND((population/(SELECT population from world where name = 'Germany'))*100,0) AS DECIMAL(3,0)),'%')
+AS percentage FROM world WHERE continent = 'Europe'
+
+SELECT name FROM world WHERE gdp > (SELECT MAX(gdp) FROM world WHERE continent = 'Europe')
+
+SELECT continent, name, area FROM world x
+WHERE area >= ALL
+(SELECT area FROM world y
+WHERE y.continent=x.continent
+AND area>0)
+
+SELECT continent, name FROM world x
+WHERE name <= ALL
+(SELECT name FROM world y
+WHERE y.continent=x.continent)
+
+SELECT name, continent, population FROM world x
+  WHERE 25000000>=ALL (SELECT population FROM world y
+                         WHERE x.continent=y.continent
+                         AND population>0)
+
+SELECT name, continent FROM world x
+WHERE population >= ALL (SELECT population*3 FROM world y WHERE x.continent = y.continent AND x.name != y.name)
+
