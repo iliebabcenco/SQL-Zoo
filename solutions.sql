98defd900f7ab85aa9b93187ee8dde44523e2f0c
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-09 17:55:30 +0300

    Add solutions for THE JOIN operation

--- solutions.sql
+++ solutions.sql
@@ -218,3 +218,14 @@ GROUP BY mdate, id
 SELECT matchid, mdate, COUNT(goal.teamid) AS goal FROM game JOIN goal ON id = matchid 
 WHERE goal.teamid = 'GER' GROUP BY matchid, mdate
 
+SELECT mdate,
+team1, 
+SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1, 
+team2,
+SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+FROM game LEFT JOIN goal ON matchid = id 
+GROUP BY mdate, matchid, team1, team2
+ORDER BY mdate, matchid, team1, team2
+  
+
+
