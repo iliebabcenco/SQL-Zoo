a5eb4b21c223ca969e72e46a6f9d07b259c8acf0
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-11 10:25:41 +0300

    Add 8 solutions for Self join challenges

--- solutions.sql
+++ solutions.sql
@@ -335,3 +335,42 @@ CASE WHEN dept = 1 THEN 'Sci'
      WHEN dept = 3 THEN 'Art'
      ELSE 'None' END
 FROM teacher
+
+--Self join
+
+SELECT COUNT(*) FROM stops
+
+SELECT id FROM stops WHERE name = 'Craiglockhart'
+
+SELECT id, name FROM stops 
+JOIN route ON stops.id = route.stop
+WHERE num = '4' AND company = 'LRT'
+
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(stop) = 2
+
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b 
+ON (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop = 149
+
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name='London Road'
+
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=115 AND b.stop = 137
+
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+ JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name='Tollcross'
